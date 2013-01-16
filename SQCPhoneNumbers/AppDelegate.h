e4c351251c533814e9d64d8b200118db9319b817
sun qichao <sunqichao@suntekiMacBook-Pro.local>
2013-01-16 22:31:29 +0800

    Initial Commit

---
+++ SQCPhoneNumbers/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SQCPhoneNumbers
+//
+//  Created by sun qichao on 13-1-16.
+//  Copyright (c) 2013年 sun qichao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
